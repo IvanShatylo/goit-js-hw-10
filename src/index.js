8811187397fc0a585c7477ef8a55de9729923e21
Ivan Shatylo <shatilo540@gmail.com>
2023-04-30 23:50:34 +0300

    add task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 import './css/styles.css';
+import { refs } from './js/refs';
+import { fetchCountries } from './js/fetch';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+refs.searchInput.addEventListener(
+  'input', debounce(e => {
+      const countryName = refs.searchInput.value.trim();
+         clearData();   
+    if (countryName !== '') {
+        fetchCountries(countryName).then(data => {      
+
+        if (data.length > 10) {
+          Notify.info(
+            'Too many matches found. Please enter a more specific name.'
+          );
+        } else if (data.length === 0) {
+          Notify.failure('Oops, there is no country with that name');
+        } else if (data.length >= 2 && data.length <= 10) {
+         
+          renderCountryList(data);
+        } else if (data.length === 1) {
+    
+          renderOneCountry(data);
+        }
+      });
+    }
+  }, DEBOUNCE_DELAY)
+);
+
+function renderCountryList(countries) {
+  const markup = countries
+    .map(country => {
+      return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${
+        country.name.official
+      }" width="30" hight="20">
+         <b>${country.name.official}</p>
+                </li>`;
+    })
+    .join('');
+  refs.countryList.innerHTML = markup;
+}
+
+function renderOneCountry(countries) {
+      const markup = countries
+        .map(country => {
+          return `<li>
+      <img src="${country.flags.svg}" alt="Flag of ${
+            country.name.official
+          }" width="30" hight="20">
+         <b>${country.name.official}</b></p>
+            <p><b>Capital</b>: ${country.capital}</p>
+            <p><b>Population</b>: ${country.population}</p>
+            <p><b>Languages</b>: ${Object.values(country.languages)} </p>
+                </li>`;
+        })
+        .join('');
+      refs.countryList.innerHTML = markup;
+}
+
+function clearData() {
+  refs.countryList.innerHTML = '';
+  refs.countryInfo.innerHTML = '';
+}
